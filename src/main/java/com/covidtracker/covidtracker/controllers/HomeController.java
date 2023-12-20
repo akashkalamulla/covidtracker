a42810fc4b166b6548c6439946cddaf639bebcbf
Akash kalamulla <akashkalamulla1@gmail.com>
2023-12-20 13:48:16 +0530

    Done creating HomeController.java and controller package and html template file

---
+++ src/main/java/com/covidtracker/covidtracker/controllers/HomeController.java
@@ -0,0 +1,29 @@
+package com.covidtracker.covidtracker.controllers;
+
+import org.springframework.ui.Model;
+import com.covidtracker.covidtracker.model.LocationStats;
+import com.covidtracker.covidtracker.services.CoronaVirusDataServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    CoronaVirusDataServices coronaVirusDataServices;
+
+    @GetMapping("/")
+    public String home(Model model){
+        List<LocationStats> allStats = coronaVirusDataServices.getAllStats();
+        int totalReportedCases = allStats.stream().mapToInt(stat->stat.getLatestTotalCases()).sum();
+        int totalNewCases = allStats.stream().mapToInt(stat->stat.getDiffFromPrevDay()).sum();
+        model.addAttribute("localtionStats ",allStats);
+        model.addAttribute("totalReportCases",totalReportedCases);
+        model.addAttribute("totalNewCases",totalNewCases);
+
+        return "home";
+    }
+}
